841e1c333665f57b0375ee1fa7c783ec207268fa
pupsione <delait.tryz000@gmail.com>
2021-06-10 15:56:23 +0200

    add timer JS

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
-window.addEventListener('DOMContentLoaded', function() {
-    
-	const tabs = document.querySelectorAll('.tabheader__item'),
-		tabsContent = document.querySelectorAll('.tabcontent'),
-		tabsParent = document.querySelector('.tabheader__items');
+window.addEventListener('DOMContentLoaded', function () {
 
-	function hideTabContent() {
-        
+    //tabs
+
+    const tabs = document.querySelectorAll('.tabheader__item'),
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelector('.tabheader__items');
+
+    function hideTabContent() {
+
         tabsContent.forEach(item => {
             item.classList.add('hide');
             item.classList.remove('show', 'fade');
@@ -14,26 +16,84 @@ window.addEventListener('DOMContentLoaded', function() {
         tabs.forEach(item => {
             item.classList.remove('tabheader__item_active');
         });
-	}
+    }
 
-	function showTabContent(i = 0) {
+    function showTabContent(i = 0) {
         tabsContent[i].classList.add('show', 'fade');
         tabsContent[i].classList.remove('hide');
         tabs[i].classList.add('tabheader__item_active');
     }
-    
+
     hideTabContent();
     showTabContent();
 
-	tabsParent.addEventListener('click', function(e) {
-		const target = e.target;
-		if(target && target.classList.contains('tabheader__item')) {
+    tabsParent.addEventListener('click', function (e) {
+        const target = e.target;
+        if (target && target.classList.contains('tabheader__item')) {
             tabs.forEach((item, i) => {
                 if (target == item) {
                     hideTabContent();
                     showTabContent(i);
                 }
             });
-		}
-	});
+        }
+    });
+
+    //timer
+
+    const deadLine = '2021-06-18';//конец таймера
+
+    function getTimeRemaining(endtime) { //данные даты от пользывателя
+        const t = Date.parse(endtime) - Date.parse(new Date()), //разница между датой пользавателя и конечной датой
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+
+        return { //записываем полученные данные в обьект
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function getZero(num) { //добавление '0' перед числом < 10
+        if (num >= 0 && num < 10) {
+            return '0' + num;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) { // полная сборка таймера
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);// частота обновления таймера
+
+        updateClock(); //чтоб скрипт не скакал из-за значений в HTML
+
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            // выводим получившие значения в DOM
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            //при окончании таймера, удаляеем его
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('.timer', deadLine);//передаю агруметы для конечной функции
+
 });
